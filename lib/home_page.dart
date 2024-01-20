ac635a2342b0a70caa714b4200f20800a43b5419
SUJAN <123sujanpr@gmail.com>
2024-01-20 13:52:21 +0545

    day 2

---
+++ lib/home_page.dart
@@ -0,0 +1,26 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final int days = 25;
+  final String name = "flutterSujan";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: 
+        Center(
+          child:
+           Text(
+            "Catalog App"
+            )),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
