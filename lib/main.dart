5794b748b4c103bae90faa2fff8ae31d806499a8
SUJAN <123sujanpr@gmail.com>
2024-01-21 15:12:17 +0545

    elevated butto,add image google font

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'pages/home_page.dart';
 
@@ -17,6 +18,9 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.red,
+        // fontFamily: GoogleFonts.aBeeZee,
+        fontFamily: GoogleFonts.lato().fontFamily,
+        // primaryTextTheme: GoogleFonts.latoTextTheme(),
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,9 +6,60 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+      color: Colors.white,
       child: Column(
         children: [
-          Image.asset("assets/image/login.png")
+          Image.asset(
+            "assets/image/login.png",
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Text(
+            "Welcome",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                  ),
+                ),
+                // SizedBox(
+                //   height: 4,
+                // ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                ElevatedButton(
+                  child: Text("Login"),
+                  style: TextButton.styleFrom(),
+                  onPressed: () {
+                    print("your are login");
+                  },
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
