6dde79aa0237696d97139b214bd97b2b16ae2a39
SUJAN <123sujanpr@gmail.com>
2024-01-22 13:16:54 +0545

    animation completed day6

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -20,7 +27,7 @@ class LoginPage extends StatelessWidget {
               height: 20.0,
             ),
             Text(
-              "Welcome",
+              "Welcome $name",
               style: TextStyle(
                 fontSize: 29,
                 fontWeight: FontWeight.bold,
@@ -39,6 +46,10 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter Username",
                       labelText: "Username",
                     ),
+                    onChanged: (value) {
+                      name = value;
+                      setState(() {});
+                    },
                   ),
                   // SizedBox(
                   //   height: 4,
@@ -49,18 +60,63 @@ class LoginPage extends StatelessWidget {
                       hintText: "Enter Password",
                       labelText: "Password",
                     ),
+                    // onChanged: (value) {
+                    //   name = value;
+                    //   setState(() {});
+                    // },
                   ),
                   SizedBox(
                     height: 40.0,
                   ),
-                  ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(minimumSize: Size(120, 40)),
-                    onPressed: () {
-                      // print("your are login");
-                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changeButton = true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(
+                        context,
+                        MyRoutes.homeRoute,
+                      );
                     },
+                    child: AnimatedContainer(
+                      duration: Duration(
+                        seconds: 1,
+                      ),
+                      width: changeButton ? 50 : 150,
+                      height: 50,
+                      // color: Colors.deepPurple,
+                      alignment: Alignment.center,
+                      child: changeButton
+                          ? Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18,
+                              ),
+                            ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        // shape:
+                        // changeButton ? BoxShape.circle : BoxShape.rectangle,
+                        borderRadius:
+                            BorderRadius.circular(changeButton ? 50 : 8),
+                      ),
+                    ),
                   ),
+                  // ElevatedButton(
+                  //   child: Text("Login"),
+                  //   style: TextButton.styleFrom(minimumSize: Size(120, 40)),
+                  //   onPressed: () {
+                  //     // print("your are login");
+                  //     Navigator.pushNamed(context, MyRoutes.homeRoute);
+                  //   },
+                  // ),
                 ],
               ),
             ),
