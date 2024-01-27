8f038443da52bf0cd005c7eff12460f78784881c
SUJAN <123sujanpr@gmail.com>
2024-01-27 20:35:09 +0545

    drawer completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+import "../widgets/drawer.dart";
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   final int days = 25;
@@ -8,19 +10,14 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: 
-        Center(
-          child:
-           Text(
-            "Catalog App"
-            )),
+        title: Center(child: Text("Catalog App")),
       ),
       body: Center(
         child: Container(
           child: Text("Welcome to $days days of flutter by $name"),
         ),
       ),
-      drawer: Drawer(),
+      drawer: MyDrawer(),
     );
   }
 }
---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final imageUrl = "https://avatars.githubusercontent.com/u/94066920?v=4";
+    return Drawer(
+      child: Container(
+        color: Colors.red,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              padding: EdgeInsets.zero,
+              child: UserAccountsDrawerHeader(
+                margin: EdgeInsets.zero,
+                // decoration: BoxDecoration(color: Colors.blueAccent),
+                accountName: Text(
+                  "Sujan Pradhan",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    letterSpacing: double.minPositive,
+                  ),
+                ),
+                accountEmail: Text("sujan@gmail.com"),
+                currentAccountPicture:
+                    CircleAvatar(backgroundImage: NetworkImage(imageUrl)),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email me",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.location_circle,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Location",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.phone,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Contact Detail",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
