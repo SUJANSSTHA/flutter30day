879164f37b82fe052c8f743d8f01b4a782780d88
SUJAN <123sujanpr@gmail.com>
2024-01-30 15:13:13 +0545

    ListView Builder, List Generate, Card & Asserts

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,48 +1,139 @@
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+    Item(
+      id: 1,
+      name: "iPhone 22 Pro",
+      desc: "Apple iPhone 22nd generation",
+      price: 200,
+      color: "#33505a",
+      image:
+          "https://nayapurano.shop/cdn/shop/files/apple-iphone-12-pro-max-1.jpg?v=1701932754&width=1445",
+    ),
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+  // Marking 'color' as required
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
 }
-
-final products = [
-  Item(
-      id: "sujan",
-      name: "iPhone 12 Pro",
-      desc: "Apple iphone 12th generation",
-      color: "#33505a",
-      image:
-          "https://unsplash.com/photos/silver-iphone-6-with-blue-case-OjMyiwfviQ4"),
-  Item(
-      id: "sujan",
-      name: "iPhone 12 Pro",
-      desc: "Apple iphone 12th generation",
-      color: "#33505a",
-      image:
-          "https://unsplash.com/photos/silver-iphone-6-with-blue-case-OjMyiwfviQ4"),
-  Item(
-      id: "sujan",
-      name: "iPhone 12 Pro",
-      desc: "Apple iphone 12th generation",
-      color: "#33505a",
-      image:
-          "https://unsplash.com/photos/silver-iphone-6-with-blue-case-OjMyiwfviQ4"),
-  Item(
-      id: "sujan",
-      name: "iPhone 12 Pro",
-      desc: "Apple iphone 12th generation",
-      color: "#33505a",
-      image:
-          "https://unsplash.com/photos/silver-iphone-6-with-blue-case-OjMyiwfviQ4"),
-  Item(
-      id: "sujan",
-      name: "iPhone 12 Pro",
-      desc: "Apple iphone 12th generation",
-      color: "#33505a",
-      image:
-          "https://unsplash.com/photos/silver-iphone-6-with-blue-case-OjMyiwfviQ4"),
-];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
 
+import "../models/catalog.dart";
 import "../widgets/drawer.dart";
+import "../widgets/item_widget.dart";
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,13 +10,20 @@ class HomePage extends StatelessWidget {
   final String name = "flutterSujan";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text("Catalog App")),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of flutter by $name"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
