95e5ba65632957f5a6bcfaa3c533859c2e73b9bb
SUJAN <123sujanpr@gmail.com>
2024-01-31 11:27:46 +0545

    assets/files/

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,38 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+import 'package:flutter/src/services/asset_bundle.dart';
 
 import "../models/catalog.dart";
 import "../widgets/drawer.dart";
 import "../widgets/item_widget.dart";
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 25;
+
   final String name = "flutterSujan";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+
+
+//! continue 4:15:56 / 8:.......
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    print(catalogJson);
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(10, (index) => CatalogModel.items[0]);
